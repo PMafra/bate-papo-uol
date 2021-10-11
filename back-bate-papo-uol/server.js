b04ef88457d26ba8438ca34ccf3215f3164d5f7f
PMafra <pmafrarm@gmail.com>
2021-10-11 00:59:50 -0300

    Participants post route and Messages post route errors treated; day js library implemented; Messages post route errors treated;

--- back-bate-papo-uol/server.js
+++ back-bate-papo-uol/server.js
@@ -1,19 +1,24 @@
 import express from "express";
 import cors from "cors";
-import fs from 'fs';
+import dayjs from "dayjs";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const participants = [];
-
 const messages = [];
 
 app.post('/participants', (req, res) => {
-    const name = req.body;
+    const name = req.body.name;
+    if (!name) {
+        res.status(400).send("Bad Request: O nome não pode ser uma string vazia");
+    }
+    if (participants.find(participant => participant.name === name)) {
+        res.status(400).send("Bad Request: Nome já existente");
+    }
     participants.push({
-        ...name,
+        name,
         lastStatus: Date.now()
     });
     messages.push({
@@ -21,9 +26,9 @@ app.post('/participants', (req, res) => {
         to: "Todos",
         text: "entra na sala...",
         type: "status",
-        time: 'HH:MM:SS'
-    })
-    res.send(name);
+        time: dayjs().format("HH:mm:ss")
+    });
+    res.sendStatus(200);
 });
 
 app.get('/participants', (req, res) => {
@@ -33,26 +38,33 @@ app.get('/participants', (req, res) => {
 app.post('/messages', (req, res) => {
     const message = req.body;
     const user = req.header("User");
+    if (!message.to || !message.text) {
+        res.status(400).send("Bad Request: String vazia encontrada");
+    }
+    if (message.type !== "message" && message.type !== "private_message") {
+        res.status(400).send("Bad Request: Tipo de mensagem errada");
+    }
+    if (!participants.find(participant => participant.name === user)) {
+        res.status(400).send("Bad Request: Remetente não existente na lista de participantes")
+    }
     messages.push({
         from: user,
         ...message,
-        time: 'HH:MM:SS'
+        time: dayjs().format("HH:mm:ss")
     })
-    res.send(message);
+    res.sendStatus(200);
 });
 
 app.get('/messages', (req, res) => {
     const user = req.header("User");
-    const messagesToSend = messages.filter(message => {
-        message.to === "todos" || message.to === user || message.from === user
-    });
-
+    const messagesToSend = messages.filter(message => 
+        message.to === "Todos" || message.to === user || message.from === user
+    );
     if (req.query.limit) {
         const messagesQty = req.query.limit;
         res.send(messagesToSend.slice(-messagesQty));
-    } else {
-        res.send(messagesToSend);
     }
+    res.send(messagesToSend);
 });
 
 app.post('/status', (req, res) => {
@@ -67,22 +79,24 @@ app.post('/status', (req, res) => {
     res.send(user);
 });
 
-setInterval(isUserInactive, 15000);
+// const isUserInactive = () => {
+//     const secondsNow = Date.now();
+//     for(let i = 0; i < participants.length; i++) {
+//         if (participants.length !== 0) {
+//             if ((secondsNow - participants[i].lastStatus)/1000 > 10) { 
+//                 participants.splice(i, 1);
+//                 messages.push({
+//                     from: participants[i].name,
+//                     to: "Todos",
+//                     text: "sai da sala...",
+//                     type: 'status', 
+//                     time: 'HH:MM:SS'
+//                 });
+//             }
+//         }
+//     }
+// }
 
-const isUserInactive = () => {
-    const secondsNow = Date.now();
-    for(let i = 0; i < participants.length; i++) { 
-        if ((secondsNow - participants[i].lastStatus)/1000 > 10) { 
-            participants.splice(i, 1);
-            messages.push({
-                from: participants[i].name,
-                to: "Todos",
-                text: "sai da sala...",
-                type: 'status', 
-                time: 'HH:MM:SS'
-            });
-        }
-    }
-}
+// setInterval(isUserInactive, 15000);
 
 app.listen(4000);
